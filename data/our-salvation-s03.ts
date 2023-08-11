67911b91017f975feed0819afeae9eb3236970d6
Timilehin Omotugba <tomotugba@afexnigeria.com>
2023-08-11 09:35:43 +0100

    update questions

--- data/our-salvation-s03.ts
+++ data/our-salvation-s03.ts
@@ -89,7 +89,60 @@ export const oss03p06 = {
     icon: Book1,
     color: "#445069",
     slug: "oss03p06",
-    questions: []
+    questions: [
+        {
+            q: "There are some _ in Christianity that you must come to honour ",
+            a: "Fundamentals",
+        },
+        {
+            q: "The man in Christ is a new _ ",
+            a: "Specie",
+        },
+        {
+            q: "The believer's relationship with God is by _ ",
+            a: "Birth",
+        },
+        {
+            q: "_ is a warfare against God",
+            a: "Sin",
+        },
+        {
+            q: "_ is no longer operating under probability",
+            a: "A Christian",
+        },
+        {
+            q: "The Bible is not a _",
+            a: "Talisman",
+        },
+        {
+            q: " _ should not make you jittery",
+            a: "That something was quoted from the Bible",
+        },
+        {
+            q: "You can deny God from _ ",
+            a: "Evidence in the Bible",
+        },
+        {
+            q: "It is the man that refuses to acknowledge the redemption of Christ that will be _ ",
+            a: "Denied",
+        },
+        {
+            q: "God set a _ around salvation",
+            a: "Boundary",
+        },
+        {
+            q: " It is too late for Jesus to say _ ",
+            a: "He doesn't know you",
+        },
+        {
+            q: "The sufferings of Christ are _ ",
+            a: "Spritual",
+        },
+        {
+            q: "_ is a statement of nature and not conduct",
+            a: "My sheep hear my voice",
+        },
+    ]
 }
 
 export const oss03p07 = {
@@ -98,7 +151,89 @@ export const oss03p07 = {
     icon: Stickynote,
     color: "#6528F7",
     slug: "oss03p07",
-    questions: []
+    questions: [
+        {
+            q: "God has placed no _ on man as touching salvation",
+            a: "Demand",
+        },
+        {
+            q: "You are not called to _ yourself as touching salvation",
+            a: "Assess",
+        },
+        {
+            q: "Self-assessment is a slippery road that leads to _",
+            a: "Condemnation",
+        },
+        {
+            q: " _ are those that reject the grace and free offer of God",
+            a: "Those under the law",
+        },
+        {
+            q: "It was _ that about the plenty laws of Moses",
+            a: "A continuous rejection of God's grace",
+        },
+        {
+            q: "What we call law is to seek _ outside _",
+            a: "God, Christ",
+        },
+        {
+            q: "_ will be seeking God outside Christ",
+            a: "Legalism",
+        },
+        {
+            q: "_ does not equate salvation.",
+            a: "Self-improvement",
+        },
+        {
+            q: "_ are steps people take to approach God",
+            a: "-",
+        },
+        {
+            q: "_ is our religion",
+            a: "Jesus",
+        },
+        {
+            q: "_ matters more than the definition",
+            a: "The explanation",
+        },
+        {
+            q: "Man has always been trying to _ to fellowship with God again",
+            a: "Cover his shame",
+        },
+        {
+            q: "Doing things to approach God will _",
+            a: "Take you farther from the same God",
+        },
+        {
+            q: "To reign, you must know that _",
+            a: "What you received is a gift",
+        },
+        {
+            q: "You miss God by _ ",
+            a: "Looking for God",
+        },
+        {
+            q: "All the blessings in Christ were received via _",
+            a: "The same event",
+        },
+        {
+            q: "God is mighty to save, just don't _",
+            a: "Run away",
+        },
+        {
+            q: "It takes _ for you not to be saved",
+            a: "So much more",
+        },
+        {
+            q: "To enter _ is a struggle because you will be labouring to _",
+            a: "Hell, Chase God away",
+        },
+        {
+            q: "_ and _ will give you labour",
+            a: "Sin, Legalism",
+        },
+
+    ]
 }
 
 
@@ -108,5 +243,74 @@ export const oss03p08 = {
     icon: BoxTick,
     color: "#45CFDD",
     slug: "oss03p08",
-    questions: []
+    questions: [
+        {
+            q: " If we have to _ for salvation then it cannot be qualified as _ ",
+            a: "Pray and Fast, A good giving",
+        },
+        {
+            q: "Salvation is good because someone _ ",
+            a: "Paid what was required",
+        },
+        {
+            q: "_ is actually a creation of man",
+            a: "Sin",
+        },
+        {
+            q: "What a lot of people are spending their strength to counterfeit is _ ",
+            a: "The Process of salvation",
+        },
+        {
+            q: "Valid salvation will be such that will be _, _ and _ by God alone",
+            a: "Initiated, Consummated and Implemented",
+        },
+        {
+            q: "The resolution of man to do it himself delayed _",
+            a: "The Incarnation",
+        },
+        {
+            q: "A thousand _ of Solomon is a thousand _ to God",
+            a: "Offering, Displeasure",
+        },
+        {
+            q: "The chariots of fire took Elijah to _",
+            a: "Hell",
+        },
+        {
+            q: "The place called Abraham's bosom is a _",
+            a: "Temporary waiting room",
+        },
+        {
+            q: "The war between God and man was _",
+            a: "One-sided",
+        },
+        {
+            q: "It was man that chose _ instead of _ ",
+            a: "Self-dependency, God-sufficiency",
+        },
+        {
+            q: "A good choir will sing _ than _",
+            a: "God's love for man, Our love for him",
+        },
+        {
+            q: "_ is the joker of Christianity",
+            a: "Forgiveness of sin",
+        },
+        {
+            q: "It is time to stop _ and start living _ ",
+            a: "Struggling, The Christian life",
+        },
+        {
+            q: "The road to eternal life is _",
+            a: "Eternal death",
+        },
+        {
+            q: "It is pride to think that your own effort can _ the work that God alone started",
+            a: "Complement",
+        },
+        {
+            q: "He is able to save _, _ and _",
+            a: "Finally, Perfectly and Completely",
+        },
+    ]
 }

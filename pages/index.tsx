ab5c07f8de120e2ff70b38727af31a8cb4550eed
Timilehin Omotugba <tomotugba@afexnigeria.com>
2023-08-25 13:55:33 +0100

    remove randomisation os questions

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ import {
 import { ArrowRight, Book1 } from "iconsax-react";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { FormEvent, useEffect, useState } from "react";
+import { FormEvent, useState } from "react";
 
 export default function Home() {
   const { query } = useRouter();
@@ -81,19 +81,12 @@ function Landing() {
 function Questions() {
   const [value, setValue] = useState("");
   const [isCheck, setIsCheck] = useState(false);
-  const [question, setQuestion] = useState<{ q: string; a: string }>({
-    q: "",
-    a: "",
-  });
+
   const { pathname, query } = useRouter();
   const { sermon, question_number = 0 } = query;
   const details = sermons.find((item) => item.slug === sermon);
   const questions = details?.questions;
-
-  useEffect(() => {
-    const q = randomNoRepeats(questions || []);
-    setQuestion(q());
-  }, [questions, question_number]);
+  const question = questions?.[Number(question_number) - 1];
 
   function checkAnswer(event: FormEvent) {
     event.preventDefault();
@@ -236,17 +229,4 @@ function NoQuestionCard() {
       </Card>
     </Container>
   );
-}
-
-function randomNoRepeats(array: { q: string; a: string }[]) {
-  var copy = array.slice(0);
-  return function () {
-    if (copy.length < 1) {
-      copy = array.slice(0);
-    }
-    var index = Math.floor(Math.random() * copy.length);
-    var item = copy[index];
-    copy.splice(index, 1);
-    return item;
-  };
 }

09f8110e6516877f1e4ed7b774f0cea8a82c2086
Timilehin Omotugba <omotugbatimilehin7@gmail.com>
2023-12-08 15:17:13 +0100

    why this error?

--- data/sda/one/part-five.ts
+++ data/sda/one/part-five.ts
@@ -1,7 +1,7 @@
 import { Timer1 } from "iconsax-react";
 
 export const part_five = {
-    title: "SDA (Incomplete)",
+    title: "SDA",
     description: "Series 1 Part 5",
     icon: Timer1,
     color: "#F7D060",

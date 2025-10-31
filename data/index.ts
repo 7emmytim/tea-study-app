922c4080771fa52fe4a42c0f89a0e6ef582b7015
Timilehin Omotugba <omotugbatimilehin7@gmail.com>
2025-10-31 08:13:54 +0100

    remove wrong gwga

--- data/index.ts
+++ data/index.ts
@@ -1,7 +1,3 @@
-import * as gods_word_gods_action_series1 from "./gods-word-gods-actions-1"
-import * as gods_word_gods_action_series2 from "./gods-word-gods-actions-2"
-import * as gods_word_gods_action_series3 from "./gods-word-gods-actions-3"
-
 // import * as our_jesus_4 from "./our-jesus-4";
 
 import * as understanding_genesis_5 from "./understanding-genesis-5";
@@ -13,42 +9,6 @@ import * as ttm2 from "./ttm2"
 // import * as tbaii from "./the-bible-and-its-inteterpretation"
 
 export const sermons = [
-  gods_word_gods_action_series1.part_one,
-  gods_word_gods_action_series1.part_two,
-  gods_word_gods_action_series1.part_three,
-  gods_word_gods_action_series1.part_four,
-  gods_word_gods_action_series1.part_five,
-  gods_word_gods_action_series2.part_one,
-  gods_word_gods_action_series2.part_two,
-  gods_word_gods_action_series2.part_three,
-  gods_word_gods_action_series2.part_four,
-  gods_word_gods_action_series2.part_five,
-  gods_word_gods_action_series2.part_six,
-  gods_word_gods_action_series2.part_seven,
-  gods_word_gods_action_series2.part_eight,
-  gods_word_gods_action_series2.part_nine,
-  gods_word_gods_action_series2.part_ten,
-  gods_word_gods_action_series2.part_eleven,
-  gods_word_gods_action_series3.part_one,
-  gods_word_gods_action_series3.part_two,
-  gods_word_gods_action_series3.part_three,
-  gods_word_gods_action_series3.part_four,
-  gods_word_gods_action_series3.part_five,
-  gods_word_gods_action_series3.part_six,
-  gods_word_gods_action_series3.part_seven,
-  gods_word_gods_action_series3.part_eight,
-  gods_word_gods_action_series3.part_nine,
-  gods_word_gods_action_series3.part_ten,
-  gods_word_gods_action_series3.part_eleven,
-  gods_word_gods_action_series3.part_twelve,
-  gods_word_gods_action_series3.part_thirteen,
-  gods_word_gods_action_series3.part_fourteen,
-  gods_word_gods_action_series3.part_fifteen,
-  gods_word_gods_action_series3.part_sixteen,
-  gods_word_gods_action_series3.part_seventeen,
-  gods_word_gods_action_series3.part_eighteen,
-  gods_word_gods_action_series3.part_nineteen,
-  gods_word_gods_action_series3.part_twenty,
   // understanding_genesis_5.part_one,
   // understanding_genesis_5.part_two,
   // understanding_genesis_5.part_three,
